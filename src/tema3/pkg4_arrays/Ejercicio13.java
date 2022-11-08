3d31b013d962487d1a8d818e178de16718b6ccf3
antonio.gimenez <antonio.gimenez@DESKTOP-9MH23G4.mshome.net>
2022-11-08 14:22:29 +0100

    Ejercicios bucles

---
+++ src/tema3/pkg4_arrays/Ejercicio13.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema3.pkg4_arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author antonio.gimenez
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s = new Scanner (System.in);
+        
+        String []alumnos = new String [5];
+        alumnos[0] = "Juan";
+        alumnos[1] = "Celia";
+        alumnos[2] = "Alfedo";
+        alumnos[3] = "Maria";
+        alumnos[4] = "Pedro";
+        double suma;
+        double [][]notas = new double [5][4];
+        
+        notas[0][0] = 5;
+        notas[0][1] = 6;
+        notas[0][2] = 7;
+        notas[1][0] = 4;
+        notas[1][1] = 2;
+        notas[1][2] = 7;
+        notas[2][0] = 6;
+        notas[2][1] = 8;
+        notas[2][2] = 9;
+        notas[3][0] = 10;
+        notas[3][1] = 8;
+        notas[3][2] = 9;
+        notas[4][0] = 4;
+        notas[4][1] = 2;
+        notas[4][2] = 4;
+        
+//        System.out.println(" E1   E2   E3   MEDIA");
+//        System.out.println("---------------------------");
+//        for (int i = 0; i < notas.length; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.print(" "+notas[i][j]+" ");  //para que salga en cuadrado es sin LN
+//            }
+//            System.out.println(" ");
+//        }
+        
+        for(int i = 0; i < notas.length; i++) {
+            suma =0;
+            for (int j = 0; j < 4; j++) {
+                suma = suma + notas[i][j];
+            }
+            notas[i][4] = suma/3;
+        }
+        
+//        System.out.println(" E1   E2   E3   MEDIA");
+//        System.out.println("---------------------------");
+//        for(int i = 0; i < notas.length; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.print(" "+notas[i][j]+" ");  //para que salga en cuadrado es sin LN
+//            }
+//            System.out.println(" ");
+//        }
+        
+        
+//        //para saber los suspensos
+//        int suspenso;
+//        for (int j = 0; j < 3; j++) {
+//            suspenso=0;
+//            for (int i = 0; i < notas.length; i++) {
+//                if(notas[i][j]<5){
+//                    suspenso++;
+//                } 
+//            }
+//            System.out.println("En el "+(j+1)+"ª evaluacion hay "+suspenso+"suspensos");
+//        }
+//    }
+//    
+//}
